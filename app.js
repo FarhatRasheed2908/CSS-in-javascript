fbd9fc194479715b9714e6107e41a959b3dbc1fc
Farhat Rasheed <farhat3822@gmail.com>
2025-01-01 21:12:28 -0800

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-function Redshow(){
-    var red = document.body
-    red.style.backgroundColor = 'red'
-}
-function blueshow(){
-    var blue = document.body
-    blue.style.backgroundColor = 'blue'
-}
-function greenshow(){
-    var green = document.body
-    green.style.backgroundColor ='green'
-}
+
+//For Task1
+// function Redshow(){
+//     var red = document.body
+//     red.style.backgroundColor = 'red'
+// }
+// function blueshow(){
+//     var blue = document.body
+//     blue.style.backgroundColor = 'blue'
+// }
+// function greenshow(){
+//     var green = document.body
+//     green.style.backgroundColor ='green'
+// }
+
+
+//For Task2
+// function change(){
+    
+//     var change = prompt('Enter any color name')
+//   var color = document.body
+//   color.style.backgroundColor =change
+//   if(  color.style.backgroundColor === ""){
+//     document.write('Invalid color')
+//   }
+// }
