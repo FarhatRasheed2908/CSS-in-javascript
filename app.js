ef09419392b158f6cb7a00ae48816a0ec2cfbcbd
Farhat Rasheed <farhat3822@gmail.com>
2025-01-01 21:05:30 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function Redshow(){
+    var red = document.body
+    red.style.backgroundColor = 'red'
+}
+function blueshow(){
+    var blue = document.body
+    blue.style.backgroundColor = 'blue'
+}
+function greenshow(){
+    var green = document.body
+    green.style.backgroundColor ='green'
+}
